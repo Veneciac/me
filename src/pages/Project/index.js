2de86a35cfd0cc9fa562888803f8a7bf35e227b5
Veneciac <veneciacalista00@gmail.com>
2019-09-02 17:12:35 +0700

    navigate link

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -1,87 +1,70 @@
 import React, { Component } from 'react';
-import { Grid, Card } from 'semantic-ui-react';
+import { Grid, Card, Image } from 'semantic-ui-react';
+import shareBoxImg from '../../assets/shareboxSS.png';
+import { Link } from 'react-router-dom';
+
 class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{
+        name: 'ShareBox',
+        description: 'ShareBox is a mini wordpress website where you can add background song to your article and maximize the experience.',
+        link: 'sharebox.veneciac.xyz',
+        video: '',
+        img: shareBoxImg
+      }, {
+        name: 'ShareBox',
+        description: 'ShareBox is a mini wordpress website where you can add background song to your article and maximize the experience.',
+        link: 'sharebox.veneciac.xyz',
+        video: '',
+        img: shareBoxImg
+      }, {
+        name: 'ShareBox',
+        description: 'ShareBox is a mini wordpress website where you can add background song to your article and maximize the experience.',
+        link: 'sharebox.veneciac.xyz',
+        video: '',
+        img: shareBoxImg
+      }]
+    }
+  }
+
+  renderCard = () => {
+    return this.state.data.map((e, i) => {
+      return (
+        <Grid.Column key={i} mobile={16} tablet={10} computer={5}>
+          <Link to={{
+            pathname:`/project/${e.name}`, 
+            detail: {
+                ...e
+            }}}>
+          <Card
+            style={{
+              minWidth: '29vw'
+            }}
+            link
+          >
+            <Image src={e.img} fluid />
+            <Card.Content>
+              <Card.Header textAlign="center" >{e.name}</Card.Header>
+              <Card.Meta>
+                {/* <span className="date">e.link</span> */}
+              </Card.Meta>
+              <Card.Description textAlign="center">{e.description}</Card.Description>
+            </Card.Content>
+          </Card>
+          </Link>
+
+      </Grid.Column>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        <Grid container columns={4} centered verticalAlign="middle">
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={8} computer={4}>
-            <Card
-              link
-              header="Rick Sanchez"
-              meta="Scientist"
-              description={[
-                'Rick is a genius scientist whose alcoholism and reckless,',
-                ' nihilistic behavior are a source of concern for his family.'
-              ].join('')}
-            />
-          </Grid.Column>
+        <Grid columns={5} centered verticalAlign="middle">
+          {this.renderCard()}
         </Grid>
       </div>
     );
--- src/pages/ProjectDetail/index.js
+++ src/pages/ProjectDetail/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class ProjectDetail extends Component {
+  componentDidMount() {
+    console.log('PARAMS', this.props.location.detail)
+  }
+  
   render() {
     return <div></div>;
   }
